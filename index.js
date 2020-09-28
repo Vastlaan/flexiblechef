ba79c3189ca289312cd0165329e50bd4b83764e4
vastlaan <vastlaan@mail.com>
2020-09-28 12:22:08 +0200

    Fix the route on server *

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ if (process.env.NODE_ENV === "production") {
     const path = require("path");
     app.use(express.static("client/build"));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
 

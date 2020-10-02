bc36399798b1bf8c3d37fae2ab901d413a4fbaab
vastlaan <vastlaan@mail.com>
2020-10-02 12:37:37 +0200

    Add build

---
+++ client/build/precache-manifest.d0bdb379dcf82aefd021d02dd3be2642.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "614abbb4ac677393b1e3",
+    "url": "/static/js/main.99430e74.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8c328a3950d8dc0195de",
+    "url": "/static/css/2.00121e17.chunk.css"
+  },
+  {
+    "revision": "8c328a3950d8dc0195de",
+    "url": "/static/js/2.1f7d8005.chunk.js"
+  },
+  {
+    "revision": "4994ccd1ebfc5d3c021b89e86f75389d",
+    "url": "/static/media/England.4994ccd1.png"
+  },
+  {
+    "revision": "2a3728c7a8c5abd7cb6ef0c5340cdb6f",
+    "url": "/static/media/Netherlands.2a3728c7.png"
+  },
+  {
+    "revision": "76925f47f9f8a3a795816223d2276c67",
+    "url": "/static/media/me.76925f47.jpg"
+  },
+  {
+    "revision": "abe86c2f52ab6c4cf66d8d5bb8f7afc0",
+    "url": "/static/media/chef-preparing.abe86c2f.png"
+  },
+  {
+    "revision": "2432b92619cf214dbd8575c9d1dfb771",
+    "url": "/static/media/catering.2432b926.png"
+  },
+  {
+    "revision": "f2835bfc578f901fe363556954dcd3a6",
+    "url": "/static/media/banqueting.f2835bfc.png"
+  },
+  {
+    "revision": "cf6a00421963274a112a731bfae8cb1d",
+    "url": "/static/media/reviews.cf6a0042.jpg"
+  },
+  {
+    "revision": "6e42e1b38ef50c1b4cdcaea27d37e6fa",
+    "url": "/index.html"
+  }
+];
